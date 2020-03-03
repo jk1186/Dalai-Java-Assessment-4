a82e9ac7760f10af1427a3274a47b6f395b2f8a1
jk1186 <jk1186@york.ac.uk>
2020-03-03 10:49:40 +0000

    Created PowerUps Class

--- core/src/com/mygdx/game/sprites/PowerUps.java
+++ core/src/com/mygdx/game/sprites/PowerUps.java
@@ -1,5 +1,15 @@
 package com.mygdx.game.sprites;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
 //TODO: Create powerups for instant health, water or immunuity. Generate random position, when collide apply effect for x seconds
 public class PowerUps extends Entity{
+    String type;
+    public PowerUps(Vector2 position, int width, int height, Texture texture, String type){
+        super(position, width, height, texture);
+        this.type = type;
+    }
+
+
 }
